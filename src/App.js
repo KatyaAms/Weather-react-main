e54dde9748cc752ec1c0d4581475f80a27d1c071
Ekaterina Bilenko <75737723+KatyaAms@users.noreply.github.com>
2021-02-03 15:14:23 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Weather from "./Weather"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+     <Weather city="Novosibirsk"/>
+     <p>
+      Hello World 
+    </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
